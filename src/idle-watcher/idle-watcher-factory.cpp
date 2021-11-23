ecf606e792855326d416f12702402f839bbdfb9e
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-11-23 11:14:54 +0800

    fix(kiran dekstop env): fix kiran desktop env compare
    - 修复kiran桌面环境变量比较错误的问题

--- src/idle-watcher/idle-watcher-factory.cpp
+++ src/idle-watcher/idle-watcher-factory.cpp
@@ -23,7 +23,7 @@ IdleWatcher* IdleWatcherFactory::createIdleWatcher()
     IdleWatcher* res = nullptr;
 
     QString desktopEnv = qgetenv("XDG_CURRENT_DESKTOP");
-    if(desktopEnv.compare("MATE",Qt::CaseInsensitive) == 0 || desktopEnv.compare("KIRAN",Qt::CaseInsensitive))
+    if (desktopEnv.compare("MATE", Qt::CaseInsensitive) == 0 || desktopEnv.compare("KIRAN", Qt::CaseInsensitive) == 0)
     {
         res = new IdleWatcherMate;
     }
