299737f342745b61a2411475e8e5e9f46337dead
liuxinhao <liuxinhao@kylinsec.com.cn>
2024-07-29 19:19:53 +0800

    fix(window cover): Add Raise screensaver for handling MapNotify and Configuring Notify events in the Root sub window
    - 订阅MapNotify以及ConfigureNotify等改变堆叠顺序的事件，将屏保窗口升起避免被其他窗口给覆盖，导致输入设备抓取但是未被渲染出来的问题
    
    Closes #41399

--- src/view/visibility-monitor.h
+++ src/view/visibility-monitor.h
@@ -16,8 +16,10 @@
 #include <QSocketNotifier>
 #include <QWidget>
 #include <QX11Info>
+#include <QSet>
 
 struct xcb_connection_t;
+struct xcb_ge_generic_event_t;
 class QSocketNotifier;
 namespace Kiran
 {
@@ -25,10 +27,9 @@ namespace ScreenSaver
 {
 /**
  * NOTE:
- * 请注意，VisibilityNotify事件只有在Marco关闭混合的情况下，XServer才会发送
- * 该类只是一个保护措施，Marco并不管理屏保窗口，XServer也只会保证XScreenSaver窗口置顶
- * 存在可能Marco同步窗口堆叠至XServer之中时，锚定窗口位于kiran-screensaver窗口之上
- * 导致Marco窗口堆叠Stack整体窗口位于kiran-screensaver窗口之上
+ * 保证 kiran-screensaver 窗口被置顶
+ * 1.在不存在混成器，XServer会发出VisibilityNotify事件通知窗口可见状态
+ * 2.订阅MapNotify,ConfigureNotify,窗口堆叠有更改时，重新置顶
 */
 class VisibilityMonitor : public QObject
 {
@@ -46,15 +47,23 @@ public:
     static VisibilityMonitor* instance();
     ~VisibilityMonitor() override;
 
-    void monitorWindow(WId wid);
+    void monitor(WId wid);
+    void unmonitor(WId wid);
 
 signals:
     void visibilityStateChanged(WId wid,VisibilityState state);
+    void restackedNeedRaise();
 
 private:
     explicit VisibilityMonitor(QObject* parent = nullptr);
     void init();
+    bool isInternal(WId window);
+    void selectSubstructureNotify();
+    void unselectSubstructureNotify();
     void handleXcbEvent();
+    void onVisibilityNotify(xcb_generic_event_t* event);
+    void onMapNotify(xcb_generic_event_t* event);
+    void onConfigureNotify(xcb_generic_event_t* event);
 
 private slots:
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
@@ -67,6 +76,7 @@ private:
     bool m_inited = false;
     xcb_connection_t* m_xcbConnection = nullptr;
     QSocketNotifier* m_xcbSocketNotifier = nullptr;
+    QSet<WId> m_windows;
 };
 }  // namespace ScreenSaver
 }  // namespace Kiran
